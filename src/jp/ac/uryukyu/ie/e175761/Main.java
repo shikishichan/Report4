6ccc54a849488a22843140b2b67c006ef472d0a0
Kisho Shiraishi <e175761@ie.u-ryukyu.ac.jp>
2017-11-14 11:21:59 +0900

    second commit

--- src/jp/ac/uryukyu/ie/e175761/Main.java
+++ src/jp/ac/uryukyu/ie/e175761/Main.java
@@ -2,8 +2,12 @@ package jp.ac.uryukyu.ie.e175761;
 
 public class Main {
     public static void main(String args[]){
-        String str = null;
-        System.out.print(str.length());
-
+        try {
+            String str = null;
+            System.out.print(str.length());
+        }catch (NullPointerException n){
+            System.out.println("NullPoが発生");
+            System.out.println(n.getMessage());
+        }
     }
 }
