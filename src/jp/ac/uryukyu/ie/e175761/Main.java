3fcf2719bd93c68372a46c91fb901081310fa745
Kisho Shiraishi <e175761@ie.u-ryukyu.ac.jp>
2017-11-14 11:35:10 +0900

    final commit

--- src/jp/ac/uryukyu/ie/e175761/Main.java
+++ src/jp/ac/uryukyu/ie/e175761/Main.java
@@ -3,8 +3,8 @@ package jp.ac.uryukyu.ie.e175761;
 public class Main {
     public static void main(String args[]){
         try {
-            String str = null;
-            System.out.print(str.length());
+            String str = "3．14";
+            double value= Double.parseDouble(str);
         }catch (NullPointerException n){
             System.out.println("NullPoが発生");
             System.out.println(n.getMessage());
