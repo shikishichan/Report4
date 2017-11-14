8262451b8ca2e0496dfdc8856deefc12e5c166e0
Kisho Shiraishi <e175761@ie.u-ryukyu.ac.jp>
2017-11-14 11:09:00 +0900

    first commit

--- src/jp/ac/uryukyu/ie/e175761/Main.java
+++ src/jp/ac/uryukyu/ie/e175761/Main.java
@@ -3,7 +3,7 @@ package jp.ac.uryukyu.ie.e175761;
 public class Main {
     public static void main(String args[]){
         String str = null;
-        System.out.println(str.length());
+        System.out.print(str.length());
 
     }
 }
